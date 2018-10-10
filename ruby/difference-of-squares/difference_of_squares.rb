2e1e68a87464c1b03335082eaa2edd157cd90994
Sam Ricotta <samanthalricotta@gmail.com>
2018-10-10 11:20:27 +1100

    difference of squares complete

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,26 +1,28 @@
-class DifferenceOfSquares
-  class Squares
-    attr_accessor :x, :sum
-    def initialize(x)
-      @x = x
-      @sum = 0
+class Squares
+  def initialize(x)
+    @x = x
+    @square_of_sum = 0
+    @sum_of_squares = 0
+  end
+  def square_of_sum
+    0.upto @x  do |e|
+     @square_of_sum += e
     end
-    def sum_of_squares
-      1.upto @x  do |e|
-        @sum += e
-      end
-      @sum
-    end
+    @square_of_sum = @square_of_sum**2
+    @square_of_sum.to_i
+  end
 
-    def square_of_sum
-      1.upto @x  do |e|
-        @sum += e**
-        end
-      end
-      @sum
+  def sum_of_squares
+    0.upto @x  do |e|
+      @sum_of_squares += e**2
     end
+    @sum_of_squares.to_i
+  end
+
+  def difference
+    sum_of_squares
+    square_of_sum
+    difference = @square_of_sum - @sum_of_squares
+    difference.to_i
   end
 end
-
-   squares = DifferenceOfSquares::Squares.new(10)
-p squares.square_of_sum
