57c77ea134f5280bec0d8d147087b11c27dc202b
Sam Ricotta <samanthalricotta@gmail.com>
2018-10-17 16:58:47 +1100

    changes made, iterating once

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,18 +1,14 @@
 class Hamming
   def self.compute(string1, string2)
     var = 0
-    if string1.size == string2.size
-      string1.each_char.with_index do |element, index|
-        string2.each_char.with_index do |e, i|
-          if index == i && element != e
-          var += 1
-          end
-        end
+    return raise ArgumentError.new("Must be same size string") unless string1.size == string2.size
+    string1_char = string1.split("")
+    string2.each_char.with_index do |element, index|
+      unless string1_char[index] == element
+        var += 1
       end
-      var
-    else
-      raise ArgumentError.new("Must be same size string")
     end
+    var
   end
 end
 
